5b5cc49f52b93b9114a82a9184cd93b089cdfcde
hbgda <dheaster123@gmail.com>
2023-06-30 14:20:58 +0100

    blanket trait impl

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,21 @@ pub mod pattern {
         Skip
     }
     pub struct Pattern(Vec<Part>);
+
+    impl Pattern {
+        pub fn new(pattern: &str) -> Pattern {
+            let parts: Vec<Part> = pattern
+                .bytes()
+                .map(|c| match c { b'?' => Part::Skip, _ => Part::Byte(c) })
+                .collect();
+
+            Pattern(parts)
+        }
+    }
 }
 
 pub trait ByteStream: Iterator<Item = u8> {}
+impl<T: Iterator<Item = u8>> ByteStream for T {}
 
 pub struct Scanner<T: ByteStream> {
     bytes: T,
@@ -29,5 +41,17 @@ impl<T: ByteStream> Iterator for Scanner<T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use crate::{Scanner, pattern::Pattern};
+
+    #[test]
+    fn test() {
+        let bytes = vec![0x0, 0x1, 0x2, 0x3];
+        let mut scanner = Scanner::new(bytes.into_iter(), Pattern::new("\0x1\0x2"));
+        assert_eq!(1usize, scanner.next().unwrap())
     }
 }
